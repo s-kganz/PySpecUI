c9d95b88b22107a65d76ee7c12df8ecf510fbd0f
s-kganz <ganzkeenan1@gmail.com>
2020-05-14 19:05:13 -0700

    create dialog for loading delimited files

--- gui.py
+++ gui.py
@@ -4,22 +4,97 @@ the application interface.
 '''
 
 import wx
-import time
-import asyncio
+from wx.lib.intctrl import IntCtrl
+from wx.lib import sized_controls
 from wxasync import AsyncBind, WxAsyncApp, StartCoroutine
 from wxmplot import PlotPanel
 
-class LoadDialog(wx.Dialog):
-    '''
-    Dialog window containing controls for loading a new data source.
-    '''
-    def __init__(self, parent=None):
-        super(LoadDialog, self).__init__(self, parent)
+import time
+import os
+import asyncio
+
+class LoadDialog(sized_controls.SizedDialog):
+
+    def __init__(self, *args, **kwargs):
+        super(LoadDialog, self).__init__(*args, **kwargs)
+        self.pane = self.GetContentsPane()
         self.InitUI()
     
     def InitUI(self):
-        pass
+        file_pane = sized_controls.SizedPanel(self.pane)
+        file_pane.SetSizerType('horizontal')
+        file_pane.SetSizerProps(halign='left')
 
+        # Add file selection control
+        wx.StaticText(file_pane, label="File: ")
+        self.fileCtrl = wx.FilePickerCtrl(file_pane)
+
+        line1 = wx.StaticLine(self.pane, style=wx.LI_HORIZONTAL)
+        line1.SetSizerProps(border=(('all', 5)), expand=True)
+
+        # Add parse settings control
+        parse_pane = sized_controls.SizedPanel(self.pane)
+        parse_pane.SetSizerType('form')
+        parse_pane.SetSizerProps(halign="left")
+        
+        # Delimiter choice
+        wx.StaticText(parse_pane, label="Delimiter: ")
+        self.delimCtrl = wx.Choice(parse_pane, choices=["Tab", "Comma", "Space"])
+
+        # Header choice
+        wx.StaticText(parse_pane, label="Parse headers: ")
+        self.headCtrl = wx.CheckBox(parse_pane)
+
+        # Comment character
+        wx.StaticText(parse_pane, label="Comment character: ")
+        self.commCtrl = wx.TextCtrl(parse_pane)
+        self.commCtrl.SetMaxLength(1)
+
+        # Frequency column
+        wx.StaticText(parse_pane, label="Index of frequency column: ")
+        self.freqIndCtrl = IntCtrl(parse_pane, min=0, limited=True, allow_none=False)
+
+        # Lines to skip
+        wx.StaticText(parse_pane, label="Lines to skip: ")
+        self.skipCtrl = IntCtrl(parse_pane, min=0, limited=True, allow_none=False)
+
+        # Second separating line
+        line2 = wx.StaticLine(self.pane, style=wx.LI_HORIZONTAL)
+        line2.SetSizerProps(border=(('all', 5)), expand=True)
+
+        # Metadata settings control
+        meta_pane= sized_controls.SizedPanel(self.pane)
+        meta_pane.SetSizerType('form')
+        meta_pane.SetSizerProps(halign="left")
+        
+        wx.StaticText(meta_pane, label="Frequency unit: ")
+        self.freqUnitCtrl = wx.TextCtrl(meta_pane)
+
+        wx.StaticText(meta_pane, label="Spectral unit: ")
+        self.specUnitCtrl = wx.TextCtrl(meta_pane)
+
+        # Third separating line
+        line3 = wx.StaticLine(self.pane, style=wx.LI_HORIZONTAL)
+        line3.SetSizerProps(border=(('all', 5)), expand=True)
+
+        # Final OK and cancel buttons
+        btns_pane = sized_controls.SizedPanel(self.pane)
+        btns_pane.SetSizerType('form')
+        btns_pane.SetSizerProps(align="center")
+
+        button_ok = wx.Button(btns_pane, wx.ID_OK, label='OK')
+        button_ok.Bind(wx.EVT_BUTTON, self.on_button)
+
+        button_cancel = wx.Button(btns_pane, wx.ID_CANCEL, label='Cancel')
+        button_cancel.Bind(wx.EVT_BUTTON, self.on_button)
+        
+        self.Fit()
+
+    def on_button(self, event):
+        if self.IsModal():
+            self.EndModal(event.EventObject.Id)
+        else:
+            self.Close()
 
 class SubPanel(wx.Panel):
     '''
@@ -132,7 +207,7 @@ class PlotRegion(SubPanel):
 class Layout(wx.Frame):
 
     def __init__(self, parent, title, datasrc=None):
-        super(Layout, self).__init__(parent, title=title)
+        super(Layout, self).__init__(parent, title=title, size=(1000, 600))
         self.datasrc = datasrc
         self.InitUI()
         self.Centre()
@@ -188,18 +263,29 @@ class Layout(wx.Frame):
         '''
         # Create dialog box
         # otherwise ask the user what new file to open
-        with wx.FileDialog(self, "Open XYZ file", wildcard="All files (*)|*.*",
-                        style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+        with LoadDialog(self, title="Load a delimited file...") as dialog:
 
-            if fileDialog.ShowModal() == wx.ID_CANCEL:
+            if dialog.ShowModal() == wx.ID_CANCEL:
                 return     # the user changed their mind
-
             # Proceed loading the file chosen by the user
-            pathname = fileDialog.GetPath()
+            path = dialog.fileCtrl.GetPath()
+            # Assemble remaining options into a dictionary
+            options = {
+                'delimChoice' : dialog.delimCtrl.GetSelection(),
+                'header' : dialog.headCtrl.GetValue(),
+                'commentChar' : dialog.commCtrl.GetValue(),
+                'freqColInd' : dialog.freqIndCtrl.GetValue(),
+                'skipCount' : dialog.skipCtrl.GetValue(),
+                'freqUnit' : dialog.freqUnitCtrl.GetValue(),
+                'specUnit' : dialog.specUnitCtrl.GetValue()
+            }
+
             try:
-                print("Load!")
-            except IOError:
-                wx.LogError("Cannot open file '%s'." % newfile)
+                traceInd = self.datasrc.addTraceFromCSV(path, options=options)
+            except IOError as e:
+                wx.LogError("Cannot open file {}.".format(path))
+                wx.LogError(str(e))
+            print("The new trace is at index {}".format(traceInd))
 
     async def SlowFunc(self, e):
         '''
@@ -211,7 +297,11 @@ class Layout(wx.Frame):
 
 
 class App(WxAsyncApp):
+    def __init__(self, datasrc=None):
+        self.datasrc = datasrc
+        super(App, self).__init__()
+
     def OnInit(self):
-        self.layout = Layout(None, title='Layout test')
+        self.layout = Layout(None, title='Layout test', datasrc=self.datasrc)
         self.layout.Show()
         return True
