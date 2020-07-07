88b8b944d109542f2401ba8e51be8080535b8700
s-kganz <ganzkeenan1@gmail.com>
2020-07-06 21:49:24 -0700

    rename functions in DataSource

--- peaks/data/datasource.py
+++ peaks/data/datasource.py
@@ -8,10 +8,11 @@ import pandas as pd
 import numpy as np
 from pubsub import pub
 from os.path import basename
+import time
 
 # NAMESPACE MODULES
-from .spec import Spectrum
-from .models import *
+from peaks.data.spectrum import Spectrum
+from peaks.data.models import *
 
 class DataSource(object):
     '''
@@ -29,12 +30,7 @@ class DataSource(object):
             "Comma": ','
         }
 
-        # Subscribe member functions
-        pub.subscribe(self.AddTraceFromCSV, 'Data.LoadCSV')
-        pub.subscribe(self.AddModel, 'Data.AddModel')
-        pub.subscribe(self.DeleteTrace, 'Data.DeleteTrace')
-
-    def GetNextId(self):
+    def _get_next_id(self):
         '''
         Used internally to generate unique IDs as traces
         are added.
@@ -42,25 +38,25 @@ class DataSource(object):
         self.trace_counter += 1
         return self.trace_counter
 
-    def AddTraceFromArrays(self, spec, freq, options = {}):
+    def add_trace_from_arrays(self, spec, freq, options = {}):
         '''
         Internal function to create a trace object from numpy arrays
         '''
         raise NotImplementedError("AddTraceFromArrays not implemented yet")
 
-    def AddTraceFromDataFrame(self, df, options = {}):
+    def add_trace_from_dataframe(self, df, options = {}):
         '''
         Create a function from a pandas dataframe.
         '''
         raise NotImplementedError("AddTraceFromDataFrame not implemented yet")
 
-    def AddManyFromDataFrame(self, df, options = {}):
+    def add_many_from_dataframe(self, df, options = {}):
         '''
         Create many Spectrum objects from a pandas dataframe.
         '''
         raise NotImplementedError("AddManyFromDataFrame not implemented yet.")
 
-    def AddTraceFromCSV(self, file, options = {}):
+    def add_trace_from_csv(self, options):
         '''
         Attempt to parse a new Spectrum object and add it to the
         data in memory.
@@ -74,29 +70,24 @@ class DataSource(object):
             'commentChar': '#',
             'freqUnit': 'x',
             'specUnit': 'y',
-            'freqColInd': 0,
-            'specColInd': 1
+            'freqCol': 0,
+            'specCol': 1
         }
         read_opts.update(options)
         # Attempt to read passed handle
         try:
             df = pd.read_csv(
-                file,
+                read_opts['file'],
                 sep=self.delim_map[read_opts["delimChoice"]],
                 skiprows=read_opts['skipCount'],
                 comment=read_opts['commentChar']
             )
         except Exception as e:
-            raise IOError("Reading {} failed:\n".format(file) + str(e))
+            raise IOError("Reading {} failed:\n".format(read_opts['file']) + str(e))
         
         # Figure out what the shape of the df is and create a trace
         if not df.ndim < 3:
-            pub.sendMessage(
-                'Logging.Error', 
-                caller='DataSource.AddTraceFromCSV', 
-                msg="Reading dataframe with more than 2 dimensions is not supported"
-            )
-            return
+            raise IOError("Only 2-dimension spectra are supported")
         elif df.shape[1] == 1:
             # Assume that 1-column frames are only spectral data, generate a dummy
             # frequency domain
@@ -107,30 +98,24 @@ class DataSource(object):
             })
         else:
             # Subset the df to only the frequency and spectral columns
-            df = df.iloc[:, [read_opts['freqColInd'], read_opts['specColInd']]]
+            df = df.iloc[:, [read_opts['freqCol'], read_opts['specCol']]]
 
         
         # Pass remaining options to the spectrum constructor
-        name = basename(file)
-        spec_id = self.GetNextId()
+        name = basename(read_opts['file'])
+        spec_id = self._get_next_id()
         self.traces.append(Spectrum(
             df,
             spec_id,
             specunit=read_opts['specUnit'],
             frequnit=read_opts['freqUnit'],
-            freqcol=read_opts["freqColInd"],
+            freqcol=read_opts["freqCol"],
             name=name
         ))
+        # Pass the data back to the application
+        return self.traces[-1]
 
-        # Pass the Spectrum to the data tab so it shows up in the UI
-        pub.sendMessage(
-            'UI.Tree.AddTrace', 
-            trace=self.traces[len(self.traces)-1], 
-            type='spec'
-        )
-        return spec_id
-
-    def AddManyFromCSV(self, file, options = {}):
+    def add_many_from_csv(self, file, options = {}):
             '''
             Parse several Spectrum objects from one delimited file
             and add them to memory.
@@ -139,12 +124,12 @@ class DataSource(object):
             '''
             raise NotImplementedError("AddManyFromCSV not implemented yet")
         
-    def AddModel(self, model):
+    def add_model(self, model):
         '''
         Add a newly created model to the internal manager and
         the data tab.
         '''
-        new_id = self.GetNextId()
+        new_id = self._get_next_id()
         model.id = new_id
         self.traces.append(model)
         pub.sendMessage(
@@ -154,7 +139,7 @@ class DataSource(object):
         )
         return new_id
 
-    def DeleteTrace(self, target_id):
+    def delete_trace(self, target_id):
         '''
         Delete a trace from the manager by its ID
         '''
@@ -164,10 +149,10 @@ class DataSource(object):
                 self.traces.pop(i)
                 break
     
-    def GetTraceByID(self, t_id):
+    def get_trace(self, t_id):
         '''
         Return spectrum object corresponding to the given id. Returns
-        None if no specturm was found
+        Raises value error if no spectrum was found.
         '''
         for trace in self.traces:
             if trace.id == t_id:
