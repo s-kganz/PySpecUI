007f7bcbdfd1cca8158aa0e913a007efb04c32af
s-kganz <ganzkeenan1@gmail.com>
2020-07-18 12:13:49 -0700

    update graph class to align with new trace functions

--- peaks/ui/datagraph.py
+++ peaks/ui/datagraph.py
@@ -16,16 +16,16 @@ class MyGraph(Graph):
         for mesh in meshes:
             self.remove_plot(mesh)
 
-    def _add_plot(self, trace=None, xmax=0, ymax=0):
+    def _add_plot(self, trace=None):
         '''
         Add a new trace object to the plot, update graph limits to include
         the entirety of the new data.
         '''
         self._update_envelope(trace)
-        super().add_plot(trace)
+        super().add_plot(trace.get_mesh())
     
     def _remove_plot(self, trace=None):
-        self.remove_plot(trace)
+        self.remove_plot(trace.get_mesh())
     
     def zoom(self, factor=2):
         '''
@@ -34,31 +34,12 @@ class MyGraph(Graph):
         self.xmax = int(self.xmax * factor)
         self.ymax = int(self.ymax * factor)
 
-    def _update_envelope(self, mesh):
+    def _update_envelope(self, trace):
         '''
         Updates the plot envelope to contain the incoming mesh, with padding.
         '''
-        new_xmin, new_xmax, new_ymin, new_ymax = self._get_mesh_envelope(mesh)
+        new_xmin, new_xmax, new_ymin, new_ymax = trace.bounds()
         self.xmin = min(self.xmin, new_xmin)
         self.xmax = max(self.xmax, new_xmax)
         self.ymin = min(self.ymin, new_ymin)
         self.ymax = max(self.ymax, new_ymax)
-
-    def _get_mesh_envelope(self, mesh, padding=0.1):
-        '''
-        Return the bounding coordinates of a mesh.
-        
-        Return order: xmin, xmax, ymin, ymax
-        '''
-        xmin = min(mesh.points, key=lambda x: x[0])[0] 
-        xmax = max(mesh.points, key=lambda x: x[0])[0] 
-        ymin = min(mesh.points, key=lambda x: x[1])[1]
-        ymax = max(mesh.points, key=lambda x: x[1])[1]
-
-        pad_y = abs(ymax * padding)
-        pad_x = 0 # abs(xmax * padding)
-
-        return (int(xmin - pad_x), 
-               int(xmax + pad_x),
-               int(ymin - pad_y),
-               int(ymax + pad_y))
