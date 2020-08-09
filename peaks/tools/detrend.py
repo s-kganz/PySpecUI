f5e97991b0a8693c2e113acb7d33cc23a226671a
s-kganz <ganzkeenan1@gmail.com>
2020-08-09 13:25:02 -0700

    add convolution functions

--- peaks/tools/detrend.py
+++ peaks/tools/detrend.py
@@ -1,6 +1,7 @@
 '''
 Functions for computing baselines of spectra.
 '''
+import scipy.signal as signal
 import numpy as np
 from .util import subset
 
@@ -29,3 +30,35 @@ def polynomial_detrend(x, y, left_bound, right_bound, degree=1, invert=False):
         x, y, 
         left_bound, right_bound, 
         degree=degree, invert=invert)
+
+def boxcar_smooth(y, winlen):
+    '''
+    Return the moving average of the input array, with winlen
+    defining the width of the window.
+    '''
+    # Normalize sum of the window
+    window = signal.windows.boxcar(winlen) / winlen
+
+    return np.convolve(y, window, mode='same')
+
+def triangular_smooth(y, winlen):
+    '''
+    Return the convolution of the input array with a triangular
+    window of width winlen.
+    '''
+    window = signal.windows.triang(winlen)
+    window /= np.sum(window)
+
+    return np.convolve(y, window, mode='same')
+
+def gaussian_smooth(y, winlen, p, sigma):
+    '''
+    Return the convolution of the input array with a gaussian
+    window of width winlen, and shape parameters p and sigma.
+
+    Note that p in this case is not the height of the peak.
+    '''
+    window = signal.windows.general_gaussian(winlen, p, sigma)
+    window /= np.sum(window)
+
+    return np.convolve(y, window, mode='same')
