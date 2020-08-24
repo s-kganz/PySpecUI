300eeae8d24ca74f3e26850c6638bd6bb1c5267f
s-kganz <ganzkeenan1@gmail.com>
2020-08-23 17:47:31 -0700

    create ToolRunInfo popup

--- peaks/ui/dialogs/common.py
+++ peaks/ui/dialogs/common.py
@@ -1,7 +1,9 @@
 from pubsub import pub
 import os
+from datetime import datetime
 
 from kivy.uix.popup import Popup
+from kivy.uix.textinput import TextInput
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.clock import Clock
 from kivy.event import EventDispatcher
@@ -107,24 +109,53 @@ class LoadDialog(Popup):
         path = os.path.join(self.filechooser.path, f)
         self.callback.text = path
 
+class DisplayTextInput(TextInput):
+    '''
+    A text input that eats all user input.
+    '''
+    def insert_text(self, *args, **kwargs):
+        return None
+    
+    def do_backspace(self, from_undo=False, mode='bkspc'):
+        return None
+    
+    def delete_selection(self, from_undo=False):
+        return None
+    
+    def cut(self, *args):
+        return None
+
+class ToolRunInfo(Popup):
+    param_info = ObjectProperty(None)
+    messages = ObjectProperty(None)
+    start_time = ObjectProperty(None)
+    end_time = ObjectProperty(None)
+
 class ToolRun(EventDispatcher):
     '''
     Retain information about a particular tool execution's parameters
     and whether the execution was successful or not.
     '''
     status = StringProperty('')
+    message_text = StringProperty('')
+    start_time = ObjectProperty(None)
+    finish_time = ObjectProperty(None)
+
     def __init__(self, ds, tool, parameters):
         self.Tool = type(tool)
         self.ds = ds
         self.name = tool.title
         self.parameters = parameters
-        self.message_text = ""
         self.status = 'Not Started'
         del tool
     
     def start(self):
         pub.sendMessage('Data.StartThread', tool_run=self)
         self.status = 'Running'
+        self.start_time = datetime.now()
+
+    def finish(self):
+        self.finish_time = datetime.now()
 
     def get_call(self):
         return lambda: self.Tool.execute(self, self.parameters)
@@ -133,7 +164,7 @@ class ToolRun(EventDispatcher):
         pub.sendMessage('Data.Post', data=data)
     
     def append_status(self, message):
-        self.message_text += "\n" + message
+        self.message_text += message + '\n'
     
     def create_dialog(self):
         t = self.Tool(self.ds)
@@ -141,3 +172,19 @@ class ToolRun(EventDispatcher):
             t.parameters[key].set_value(self.parameters[key])
         t.open()
 
+    def create_info_dialog(self):
+        tri = ToolRunInfo(title='Results of {}'.format(self.name))
+        # Make start/end time labels
+        start_text = 'Started at {}'.format(self.start_time.strftime('%H:%M:%S'))
+        finish_text = '{} at {}'.format(self.status, self.finish_time.strftime('%H:%M:%S')) \
+                      if self.status in ('Failed', 'Succeeded') else ''
+        tri.start_time.text = start_text
+        tri.end_time.text = finish_text
+        # Make parameter text
+        param_info_lines = [
+            '{}:{}'.format(key, str(self.parameters[key])) for key in self.parameters
+        ]
+        tri.param_info.text = '\n'.join(param_info_lines)
+        # Make message text
+        tri.messages.text = self.message_text
+        tri.open()
